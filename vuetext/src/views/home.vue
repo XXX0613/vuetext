76e16d1c3bd66f04591f595736f5b13c836b4fa4
XXX0613 <2858295997@qq.com>
2025-10-16 16:06:24 +0800

    vuetext

---
+++ vuetext/src/views/home.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="home-container">
+        <h1>Vue3教学</h1>
+        <hr>
+        <Mobanyufa></Mobanyufa>
+    </div>
+</template>
+
+
+<script>
+import Mobanyufa from '../components/mobanyufa.vue'
+
+export default {
+    name: 'home',
+    components: {
+        Mobanyufa
+    },
+}
+</script>
+
+<style scoped>
+.home-container {
+    position: relative;
+    z-index: 10;
+}
+
+h1 {
+    z-index: 11;
+    color: aqua;
+}
+
+</style>
